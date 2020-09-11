9ef9841ab3e36265bcc804a3f3426d23130884a7
JonathanDombrowski <jdombrowski@gladly.com>
2020-09-11 09:38:29 -0700

    extract seed data, remove express router, clean index

---
+++ server/seed.js
@@ -0,0 +1,53 @@
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const chirps = [
+  {
+    id: 1,
+    content: 'Starkist and Sunkist are very similar and very different',
+    user_id: 1,
+  },
+  {
+    id: 2,
+    content:
+      "Why didn't Voldemort just apparate into Harry's room and shoot him with a gun?",
+    user_id: 2,
+  },
+  {
+    id: 3,
+    content: 'Seattle Ice Cream politics have claimed another life',
+    user_id: 3,
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    username: 'HarryPuber',
+    bio: 'Just a boy whose only weakness is bullets',
+  },
+  {
+    id: 2,
+    username: 'Voldewart',
+    bio: 'Just a bullet whose only weakness is ecological decline',
+  },
+  {
+    id: 3,
+    username: 'roobercuber',
+    bio: 'former barista boy',
+  },
+];
+
+export default {
+  books,
+  chirps,
+  users,
+};
